05b0fbfc279d8347a2968baedfa732687c1e3755
fasaldana <fernando_s_98@hotmail.com>
2022-08-10 00:57:41 -0500

    readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Message.create(text: 'Hello 1')
+Message.create(text: 'Hello 1 ')
 Message.create(text: 'Hello 2')
 Message.create(text: 'Hello 3')
 Message.create(text: 'Hello 4')
