ada85faad8c390839bfede8cdada75fbf7f8154d
fasaldana <fernando_s_98@hotmail.com>
2022-08-09 23:53:56 -0500

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Message.create(text: 'Hello 1')
+Message.create(text: 'Hello 2')
+Message.create(text: 'Hello 3')
+Message.create(text: 'Hello 4')
+Message.create(text: 'Hello 5')
