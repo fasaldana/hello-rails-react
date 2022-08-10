eab0c79b33280549ffc9b0832e31143294bb288e
fasaldana <fernando_s_98@hotmail.com>
2022-08-10 00:20:43 -0500

    Add front-end

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,13 @@
-class Api::V1::MessagesController < ApplicationController
-    def index
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      def index
         random = Random.new
         @message = Message.find(random.rand(1..Message.count)).text
         render json: @message
+      end
     end
+  end
 end
